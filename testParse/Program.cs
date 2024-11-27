e6e25d97f693e5b8c4c5c8b5a93facd886d7f31e
9t0z1k <yatoz0r@gmail.com>
2024-11-28 02:44:04 +0500

    upd

--- testParse/Program.cs
+++ testParse/Program.cs
@@ -5,21 +5,29 @@ class Program
     static void Main()
     {
         var executor = new CommandExecutor();
-        executor.SetText("Привет, мир!");
+        Console.WriteLine("Напишите строку: ");
+        executor.SetText(Console.ReadLine());
+        var fileCommands = File.ReadAllText("commands.txt");
+        var commands = fileCommands.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        /*   List<string> commands;
 
-        var commands = new List<string>
-        {
-            "copy 1,3",
-            "insert \"hello\" 1",
-            "paste 6",
-            "undo",
-            "redo",
-            "delete 2,7",
-            "undo",
-            "undo",
-            "redo",
-            "redo"
-        };
+           foreach (var command in fileCommands)
+           {
+               commands = command;
+           }
+           *//*v*//*ar commands = new List<string>
+           {
+               "copy 1,3",
+               "insert \"hello\" 1",
+               "paste 6",
+               "undo",
+               "redo",
+               "delete 2,7",
+               "undo",
+               "undo",
+               "redo",
+               "redo"
+           };*/
 
         executor.LoadCommands(commands);
         Console.WriteLine($"Final text: {executor.GetFinalText()}");
